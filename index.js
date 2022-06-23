e9d2daf0d8f9af19265e5e4730c0c6d4eeaab491
Shahzad <mech1090@gmail.com>
2022-06-22 19:28:00 -0600

    main page setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:ture}))
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.send('OOOK')
+})
+
+app.get('*',(req,res)=>{
+    res.send('BAD_REQUUEST')
+})
+
+port = config.get('port') || 8080
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
