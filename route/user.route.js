0c34aa7bb81568a1d7a40b4ed64f521afe0f3571
Shahzad <mech1090@gmail.com>
2022-06-22 20:01:00 -0600

    logic remaining

--- route/user.route.js
+++ route/user.route.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/user.controller')
 
-router.get('/',userController.getLoginFrom)
-router.post('/',userController.login)
-router.get('/',userController.getSignupFrom)
-router.post('/',userController.signup)
+router.get('/login',userController.getLoginForm)
+router.post('/login',userController.login)
+router.get('/signup',userController.getSignupForm)
+router.post('/signup',userController.signup)
 
 module.exports = router
